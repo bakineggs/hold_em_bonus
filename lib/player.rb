bdfea9d0e8d8870d3cf6f5be341d29d16aa83298
Dan Barry <dan@bakineggs.com>
2008-12-25 23:09:05 -0600

    only bet ace high on the turn if the board is paired

--- lib/player.rb
+++ lib/player.rb
@@ -34,7 +34,7 @@ class Player
   def bet_river?(hole_cards, board_cards)
     hand = Poker::Hand.new *(hole_cards + board_cards)
     [
-      hole_cards.any?{|card| card.face == 'Ace'},
+      Poker::Hand.new(*board_cards).pair? && hole_cards.any?{|card| card.face == 'Ace'},
       Poker::Hand.new(*hole_cards).pair?,
       hole_cards.any?{|hole_card| board_cards.any?{|board_card| hole_card.face == board_card.face}},
       hand.straight?,
