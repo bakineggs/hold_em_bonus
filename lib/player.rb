4e400bcfd81b4f67c1bea1f49a558172103cfd2f
Dan Barry <dan@bakineggs.com>
2008-12-25 23:08:37 -0600

    only bet a pair if a hole card is included

--- lib/player.rb
+++ lib/player.rb
@@ -21,14 +21,24 @@ class Player
   end
 
   def bet_turn?(hole_cards, board_cards)
-    return true if hole_cards.any?{|card| card.face == 'Ace'}
     hand = Poker::Hand.new *(hole_cards + board_cards)
-    hand.pair? || hand.straight? || hand.flush?
+    [
+      hole_cards.any?{|card| card.face == 'Ace'},
+      Poker::Hand.new(*hole_cards).pair?,
+      hole_cards.any?{|hole_card| board_cards.any?{|board_card| hole_card.face == board_card.face}},
+      hand.straight?,
+      hand.flush?
+    ].any?
   end
 
   def bet_river?(hole_cards, board_cards)
-    return true if hole_cards.any?{|card| card.face == 'Ace'}
     hand = Poker::Hand.new *(hole_cards + board_cards)
-    hand.pair? || hand.straight? || hand.flush?
+    [
+      hole_cards.any?{|card| card.face == 'Ace'},
+      Poker::Hand.new(*hole_cards).pair?,
+      hole_cards.any?{|hole_card| board_cards.any?{|board_card| hole_card.face == board_card.face}},
+      hand.straight?,
+      hand.flush?
+    ].any?
   end
 end
