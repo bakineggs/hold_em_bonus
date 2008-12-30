1ac3436134d9a6b7311c60a62489a2d2c46e8fa5
Dan Barry <dan@bakineggs.com>
2008-12-30 15:56:16 -0600

    track highest and lowest balance

--- hold_em_bonus.rb
+++ hold_em_bonus.rb
@@ -11,10 +11,27 @@ players = Hash[*Player.TYPES.map do |type|
 end.flatten]
 game = Game.new(players.values)
 
+highest = {}
+lowest = {}
+players.each do |type, player|
+  highest[player] = 0
+  lowest[player] = 0
+end
+
 1000.times do
   game.play_hand
+  players.each do |type, player|
+    if player.balance > highest[player]
+      highest[player] = player.balance
+    elsif player.balance < lowest[player]
+      lowest[player] = player.balance
+    end
+  end
 end
 
 players.each do |type, player|
-  puts "#{type}: #{player.balance}"
+  puts "#{type}:"
+  puts "  Balance: #{player.balance}"
+  puts "  Highest: #{highest[player]}"
+  puts "  Lowest: #{lowest[player]}"
 end
