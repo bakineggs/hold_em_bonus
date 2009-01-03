36955bd8e1ee44603ab47c71d2b01d63a274fead
Dan Barry <dan@bakineggs.com>
2009-01-03 06:19:14 -0600

    refactor PairsWithBoardDrawLogic

--- lib/players/pairs_with_board_draw_logic.rb
+++ lib/players/pairs_with_board_draw_logic.rb
@@ -1,41 +1,84 @@
 class PairsWithBoardDrawLogic < Player
   def bet_turn?(hole_cards, board_cards)
+    @hole_cards = hole_cards
+    @board_cards = board_cards
     [
-      highest_card?(hole_cards, board_cards),
-      pair?(hole_cards, board_cards),
-      draws?(Poker::Hand.new(*(hole_cards + board_cards)))
+      highest_card?,
+      pair?,
+      draws?(hand)
     ].any?
   end
 
   def bet_river?(hole_cards, board_cards)
-    hand = Poker::Hand.new *(hole_cards + board_cards)
-    hole = Poker::Hand.new *hole_cards
-    board = Poker::Hand.new *board_cards
+    @hole_cards = hole_cards
+    @board_cards = board_cards
     [
-      highest_card?(hole_cards, board_cards) && !draws?(board),
-      pair?(hole_cards, board_cards) && !draws?(board),
-      hole.pair? && hole_cards.first.value >= board_cards.sort[2].value,
-      !hole.pair? && pair?(hole_cards, board_cards.sort[2..3]),
-      hole.pair? && hole_cards.first.value >= board_cards.sort[0].value && !strong_draws?(board),
-      !hole.pair? && pair?(hole_cards, board_cards) && !strong_draws?(board),
+      (highest_card? || pair?) && !draws?(board),
+      second_pair?,
+      fourth_pair? && !strong_draws?(board),
       hand.straight?,
       hand.flush?
     ].any?
   end
 
   private
-    def highest_card?(hole_cards, board_cards)
-      (14-board_cards.length..14).any? do |value|
-        hole_cards.any?{|card| card.value == value} &&
+    def hand(cards = nil)
+      cards ||= @hole_cards + @board_cards
+      @hands ||= {}
+      @hands[cards] ||= Poker::Hand.new *cards
+    end
+
+    def hole
+      hand(@hole_cards)
+    end
+
+    def board
+      hand(@board_cards)
+    end
+
+    def highest_card?
+      (14-@board_cards.length..14).any? do |value|
+        @hole_cards.any?{|card| card.value == value} &&
         (value+1..14).all? do |higher|
-          board_cards.any?{|card| card.value == higher}
+          @board_cards.any?{|card| card.value == higher}
         end
       end
     end
 
-    def pair?(hole_cards, board_cards)
-      Poker::Hand.new(*hole_cards).pair? ||
-      hole_cards.any?{|hole_card| board_cards.any?{|board_card| hole_card.face == board_card.face}}
+    def first_pair?
+      nth_pair?(0)
+    end
+
+    def second_pair?
+      nth_pair?(1)
+    end
+
+    def third_pair?
+      nth_pair?(2)
+    end
+
+    def fourth_pair?
+      nth_pair?(3)
+    end
+
+    def nth_pair?(n)
+      values = @board_cards.map{|card| card.value}.uniq.sort.reverse
+      n = values.length - 1 if n >= values.length
+      paired_hole_values.any? do |value|
+        value >= values[n]
+      end
+    end
+
+    def pair?
+      paired_hole_values.length > 0
+    end
+
+    def paired_hole_values
+      cards = @hole_cards + @board_cards
+      @paired_hole_values ||= {}
+      @paired_hole_values[cards] ||= [@hole_cards.select do |hole_card|
+        @board_cards.find{|board_card| hole_card.value == board_card.value}
+      end.map{|card| card.value}, hole.pair? ? @hole_cards.first.value : nil].flatten.uniq.compact
     end
 
     def strong_draws?(hand)
